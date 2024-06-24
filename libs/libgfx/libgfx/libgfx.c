5db0db30c7441618984d04bf62b9e2b1dbc9fd21
Simon "Tenry" Burchert <contact@simon-burchert.com>
2024-06-24 03:21:58 +0200

    Add libgfx and gfxtool

---
+++ libs/libgfx/libgfx/libgfx.c
@@ -0,0 +1,389 @@
+#include <stdio.h>
+#include <string.h>
+
+#include <libgfx/libgfx.h>
+
+static const char *errorMessage = NULL;
+
+#pragma pack(push, 1)
+typedef struct {
+  char signature[4];
+  uint16_t version;
+  uint16_t size; // >= sizeof(libgfx_FileHeader)
+  uint16_t width;
+  uint16_t height;
+  uint8_t format : 4;
+  uint8_t type : 4;
+  uint16_t numFrames;
+  uint16_t numTiles;
+  uint16_t numAnimations; // for tileset, number of animated tiles
+  uint16_t numPalettes; // can be 0 if no palette is included
+} libgfx_FileHeader;
+#pragma pack(pop)
+
+static int getBitsPerPixel(libgfx_ColorFormat colorFormat) {
+  int bitsPerPixel = 8;
+
+  switch (colorFormat) {
+    case LIBGFX_FORMAT_4BIT_INDEXED:
+    case LIBGFX_FORMAT_4BIT_GRAYSCALE:
+      bitsPerPixel = 4;
+      break;
+    case LIBGFX_FORMAT_8BIT_INDEXED:
+    case LIBGFX_FORMAT_8BIT_GRAYSCALE:
+      bitsPerPixel = 8;
+      break;
+    case LIBGFX_FORMAT_16BIT_RGBA:
+      bitsPerPixel = 16;
+      break;
+    case LIBGFX_FORMAT_24BIT_RGB:
+      bitsPerPixel = 24;
+      break;
+    case LIBGFX_FORMAT_32BIT_RGBA:
+      bitsPerPixel = 32;
+      break;
+  }
+
+  return bitsPerPixel;
+}
+
+static inline float getBytesPerPixel(libgfx_ColorFormat colorFormat) {
+  return ((float) getBitsPerPixel(colorFormat)) / 8;
+}
+
+static int getBytesPerFrame(libgfx_Image *image) {
+  return image->width * image->height * getBytesPerPixel(image->colorFormat);
+}
+
+static int readFileCallback(void *dest, size_t size, void *userData) {
+  FILE *file = (FILE *) userData;
+  return fread(dest, size, 1, file) * size;
+}
+
+static int readMemCallback(void *dest, size_t size, void *userData) {
+  const uint8_t **srcPointer = (const uint8_t **) userData;
+  
+  const uint8_t *src = *srcPointer;
+  memcpy(dest, src, size);
+
+  srcPointer += size;
+
+  return size;
+}
+
+static int writeFileCallback(const void *src, size_t size, void *userData) {
+  FILE *file = (FILE *) userData;
+  return fwrite(src, size, 1, file) * size;
+}
+
+static int writeMemCallback(const void *src, size_t size, void *userData) {
+  uint8_t **destPointer = (uint8_t **) userData;
+  
+  uint8_t *dest = *destPointer;
+  memcpy(dest, src, size);
+
+  destPointer += size;
+
+  return size;
+}
+
+libgfx_Image *libgfx_loadImageFromFile(const char *filename) {
+  FILE *file = fopen(filename, "rb");
+
+  if (!file) {
+    errorMessage = "Couldn't open file";
+    return NULL;
+  }
+
+  libgfx_Image *image = libgfx_loadImageFromCallback(readFileCallback, file);
+  fclose(file);
+  return image;
+}
+
+libgfx_Image *libgfx_loadImageFromMemory(const void *buffer, size_t bufferSize) {
+  return libgfx_loadImageFromCallback(readMemCallback, &buffer);
+}
+
+libgfx_Image *libgfx_loadImageFromCallback(libgfx_ReadCallback read, void *userData) {
+  libgfx_FileHeader header;
+  libgfx_Image *image = NULL;
+
+  if (read(&header, sizeof(libgfx_FileHeader), userData) == 0) {
+    errorMessage = "Couldn't read GFX header";
+    return NULL;
+  }
+
+  if (strncmp("AGFX", header.signature, 4) != 0) {
+    errorMessage = "Not a GFX file (invalid signature)";
+    return NULL;
+  }
+
+  if (header.version != 1) {
+    errorMessage = "Unsupported version";
+    return NULL;
+  }
+
+  if (header.size != sizeof(header)) {
+    errorMessage = "Incompatible header size";
+    return NULL;
+  }
+
+  if (header.format > LIBGFX_FORMAT_32BIT_RGBA) {
+    errorMessage = "Invalid color format";
+    return NULL;
+  }
+
+  if (header.type > LIBGFX_TYPE_TILESET) {
+    errorMessage = "Invalid type";
+    return NULL;
+  }
+
+  if (header.type == LIBGFX_TYPE_NONE) {
+    if (header.numFrames > 0 || header.numAnimations > 0) {
+      errorMessage = "Frames or animations not allowed for palette only";
+      return NULL;
+    }
+  } else {
+    if (header.numFrames == 0) {
+      errorMessage = "No frames included";
+      return NULL;
+    }
+  }
+
+  image = libgfx_createImage(header.width, header.height, header.format, header.type);
+
+  libgfx_allocFrames(image, header.numFrames);
+  if (read(image->frames, image->numFrames * getBytesPerFrame(image), userData) == 0) {
+    libgfx_freeImage(image);
+    errorMessage = "Error reading frames";
+    return NULL;
+  }
+
+  libgfx_allocAnimations(image, header.numAnimations);
+
+  for (int i = 0; i < image->numAnimations; ++i) {
+    libgfx_Animation *anim = libgfx_getAnimation(image, i);
+
+    uint16_t tileIndex;
+    read(&tileIndex, sizeof(tileIndex), userData);
+
+    uint8_t numFrames;
+    read(&numFrames, sizeof(numFrames), userData);
+
+    libgfx_allocAnimation(image, anim, numFrames);
+
+    anim->tileIndex = tileIndex;
+
+    for (int f = 0; f < numFrames; ++f) {
+      uint16_t frameIndex;
+      if (read(&frameIndex, sizeof(frameIndex), userData) == 0) {
+        libgfx_freeImage(image);
+        errorMessage = "Error reading animation";
+        return NULL;
+      }
+      anim->frameIndices[i] = f;
+    }
+  }
+
+  libgfx_allocPalettes(image, header.numPalettes);
+
+  for (int i = 0; i < header.numPalettes; ++i) {
+    libgfx_Palette *pal = libgfx_getPalette(image, i);
+
+    uint8_t format;
+    uint8_t numColors;
+    if (read(&format, sizeof(format), userData) == 0) {
+      libgfx_freeImage(image);
+      errorMessage = "Error reading palette";
+      return NULL;
+    }
+    if (read(&numColors, sizeof(numColors), userData) == 0) {
+      libgfx_freeImage(image);
+      errorMessage = "Error reading palette";
+      return NULL;
+    }
+
+    pal->numColors = (numColors > 0 ? numColors : 256);
+
+    libgfx_allocPalette(image, pal, format, pal->numColors);
+    if (read(pal->colors, pal->numColors * getBytesPerPixel(pal->colorFormat), userData) == 0) {
+      libgfx_freeImage(image);
+      errorMessage = "Error reading palette";
+      return NULL;
+    }
+  }
+
+  return image;
+}
+
+int libgfx_writeImageToFile(libgfx_Image *image, const char *filename) {
+  FILE *file = fopen(filename, "wb");
+  int result = libgfx_writeImageToCallback(image, writeFileCallback, file);
+  fclose(file);
+
+  return result;
+}
+
+int libgfx_writeImageToMemory(libgfx_Image *image, void *buffer, size_t bufferSize) {
+  return libgfx_writeImageToCallback(image, writeMemCallback, &buffer);
+}
+
+int libgfx_writeImageToCallback(libgfx_Image *image, libgfx_WriteCallback write, void *userData) {
+  libgfx_FileHeader header;
+
+  strncpy(header.signature, "AGFX", 4);
+  header.version = 1;
+  header.size = sizeof(header);
+  header.width = image->width;
+  header.height = image->height;
+  header.format = image->colorFormat;
+  header.type = image->type;
+  header.numFrames = image->numFrames;
+  header.numTiles = image->numTiles || image->numFrames;
+  header.numAnimations = image->numAnimations;
+  header.numPalettes = image->numPalettes;
+  write(&header, sizeof(header), userData);
+
+  int bytesPerFrame = getBytesPerFrame(image);
+  write(image->frames, image->numFrames * bytesPerFrame, userData);
+
+  for (int i = 0; i < image->numAnimations; ++i) {
+    libgfx_Animation *anim = libgfx_getAnimation(image, i);
+    uint16_t tileIndex = anim->tileIndex;
+    uint8_t numFrames = anim->numFrames;
+    write(&tileIndex, sizeof(tileIndex), userData);
+    write(&numFrames, sizeof(numFrames), userData);
+
+    for (int f = 0; f < anim->numFrames; ++f) {
+      uint16_t frameIndex = anim->frameIndices[f];
+      write(&frameIndex, sizeof(frameIndex), userData);
+    }
+  }
+
+  for (int i = 0; i < image->numPalettes; ++i) {
+    libgfx_Palette *pal = libgfx_getPalette(image, i);
+    uint8_t format = pal->colorFormat;
+    uint8_t numColors = pal->numColors;
+    write(&format, sizeof(format), userData);
+    write(&numColors, sizeof(numColors), userData);
+    write(pal->colors, pal->numColors * getBytesPerPixel(pal->colorFormat), userData);
+  }
+  
+  return 0;
+}
+
+libgfx_Image *libgfx_createImage(int width, int height, libgfx_ColorFormat colorFormat, libgfx_ImageType type) {
+  libgfx_Image *image = malloc(sizeof(libgfx_Image));
+
+  image->width = width;
+  image->height = height;
+  image->colorFormat = colorFormat;
+  image->type = type;
+  image->numFrames = 0;
+  image->numTiles = 0;
+  image->numAnimations = 0;
+  image->numPalettes = 0;
+  image->frames = NULL;
+  image->animations = NULL;
+  image->palettes = NULL;
+
+  return image;
+}
+
+int libgfx_allocFrames(libgfx_Image *image, int numFrames) {
+  int bytesPerFrame = getBytesPerFrame(image);
+  image->numFrames = numFrames;
+  image->frames = calloc(numFrames, bytesPerFrame);
+
+  return 0;
+}
+
+int libgfx_allocAnimations(libgfx_Image *image, int numAnimations) {
+  image->numAnimations = numAnimations;
+  image->animations = calloc(numAnimations, sizeof(libgfx_Animation));
+
+  return 0;
+}
+
+int libgfx_allocPalettes(libgfx_Image *image, int numPalettes) {
+  image->numPalettes = numPalettes;
+  image->palettes = calloc(numPalettes, sizeof(libgfx_Palette));
+
+  return 0;
+}
+
+int libgfx_allocAnimation(libgfx_Image *image, libgfx_Animation *animation, int numFrames) {
+  animation->tileIndex = 0;
+  animation->numFrames = numFrames;
+  animation->frameIndices = calloc(sizeof(int), numFrames);
+
+  return 0;
+}
+
+int libgfx_allocPalette(libgfx_Image *image, libgfx_Palette *palette, libgfx_ColorFormat colorFormat, int numColors) {
+  int bytesPerColor = getBytesPerPixel(colorFormat);
+  
+  palette->colorFormat = colorFormat;
+  palette->numColors = numColors;
+  palette->colors = calloc(numColors, bytesPerColor);
+
+  return 0;
+}
+
+void *libgfx_getFramePointer(libgfx_Image *image, int frameIndex) {
+  return image->frames + getBytesPerFrame(image) * frameIndex;
+}
+
+libgfx_Animation *libgfx_getAnimation(libgfx_Image *image, int index) {
+  return image->animations + index * sizeof(libgfx_Animation);
+}
+
+libgfx_Palette *libgfx_getPalette(libgfx_Image *image, int index) {
+  return image->palettes + index * sizeof(libgfx_Palette);
+}
+
+void libgfx_freeImage(libgfx_Image *image) {
+  if (image->frames) {
+    free(image->frames);
+  }
+
+  if (image->animations) {
+    for (int i = 0; i < image->numAnimations; ++i) {
+      libgfx_Animation *anim = libgfx_getAnimation(image, i);
+      free(anim->frameIndices);
+    }
+
+    free(image->animations);
+  }
+
+  if (image->palettes) {
+    for (int i = 0; i < image->numPalettes; ++i) {
+      libgfx_Palette *pal = libgfx_getPalette(image, i);
+      free(pal->colors);
+    }
+
+    free(image->palettes);
+  }
+
+  free(image);
+}
+
+libgfx_4BitPixel *libgfx_to4BitPixels(void *pixels) {
+  return (libgfx_4BitPixel *) pixels;
+}
+
+libgfx_16BitPixel *libgfx_to16BitPixels(void *pixels) {
+  return (libgfx_16BitPixel *) pixels;
+}
+
+libgfx_24BitPixel *libgfx_to24BitPixels(void *pixels) {
+  return (libgfx_24BitPixel *) pixels;
+}
+
+libgfx_32BitPixel *libgfx_to32BitPixels(void *pixels) {
+  return (libgfx_32BitPixel *) pixels;
+}
+
+const char *libgfx_getError() {
+  return errorMessage;
+}
---
+++ libs/libgfx/libgfx/libgfx.h
@@ -0,0 +1,297 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include <stdint.h>
+#include <stdlib.h>
+
+typedef enum {
+  /**
+   * @brief Image is encoded in 4 bits per pixel (16 colors).
+   *
+   * Each byte represents 2 color indices.
+   */
+  LIBGFX_FORMAT_4BIT_INDEXED = 0,
+
+  /**
+   * @brief Image is encoded in 8 bits per pixel (256 colors).
+   *
+   * Each byte represents 1 color index.
+   */
+  LIBGFX_FORMAT_8BIT_INDEXED = 1,
+
+  /**
+   * @brief Image is encoded in 4 bits per pixel (16 levels).
+   *
+   * Each byte represents 2 values.
+   */
+  LIBGFX_FORMAT_4BIT_GRAYSCALE = 2,
+  
+  /**
+   * @brief Image is encoded in 8 bits per pixel (256 levels).
+   *
+   * Each byte represents 1 value.
+   */
+  LIBGFX_FORMAT_8BIT_GRAYSCALE = 3,
+
+  /**
+   * @brief Image is encoded in 16 bits per pixel.
+   *
+   * Red, green and blue are encoded in 5 bits each (32 levels) and alpha is 1 bit.
+   */
+  LIBGFX_FORMAT_16BIT_RGBA = 4,
+
+  /**
+   * @brief Image is encoded in 24 bits per pixel.
+   *
+   * Red, green and blue are encoded in 8 bits each.
+   */
+  LIBGFX_FORMAT_24BIT_RGB = 5,
+
+  /**
+   * @brief Image is encoded in 24 bits per pixel.
+   *
+   * Red, green, blue and alpha are encoded in 8 bits each.
+   */
+  LIBGFX_FORMAT_32BIT_RGBA = 6,
+} libgfx_ColorFormat;
+
+typedef enum {
+  /**
+   * @brief Useful if the GFX file only contains palettes.
+   */
+  LIBGFX_TYPE_NONE = 0,
+  LIBGFX_TYPE_IMAGE = 1,
+  LIBGFX_TYPE_TILESET = 2,
+} libgfx_ImageType;
+
+typedef struct {
+  /**
+   * @brief Tile index to animate. 0 refers to the first defined tile.
+   */
+  int tileIndex;
+  int numFrames;
+  /**
+   * @brief List of frame or tile indices to use. 0 refers to the first defined frame.
+  */
+  int *frameIndices;
+} libgfx_Animation;
+
+typedef struct {
+  libgfx_ColorFormat colorFormat;
+  int numColors;
+  void *colors;
+} libgfx_Palette;
+
+typedef struct {
+  /**
+   * @brief Width of a single frame or tile in pixels.
+   */
+  int width;
+
+  /**
+   * @brief Height of a single frame or tile in pixels.
+   */
+  int height;
+
+  /**
+   * @brief Color format used.
+   */
+  libgfx_ColorFormat colorFormat;
+
+  /**
+   * @brief Type of image.
+   */
+  libgfx_ImageType type;
+
+  /**
+   * @brief Number of frames.
+   */
+  int numFrames;
+
+  /**
+   * @brief Number of frames that are used as the base tiles.
+   *
+   * Any frames above this numbers are meant as animation frames.
+   */
+  int numTiles;
+
+  /**
+   * @brief Number of animations.
+   *
+   * For image, animations will refer to the frames already defined.
+   *
+   * For tileset, for each animation, a tile index is specified and additional
+   * frame data is provided.
+   */
+  int numAnimations;
+
+  /**
+   * @brief Number of palettes included.
+   */
+  int numPalettes;
+
+  /**
+   * @brief List of pointers for frame or tile data.
+   *
+   * The size of each frame in bytes depends on the width, height and format of
+   * the image.
+   */
+  void *frames;
+
+  libgfx_Animation *animations;
+
+  libgfx_Palette *palettes;
+} libgfx_Image;
+
+#pragma pack(push, 1)
+typedef struct {
+  uint8_t first : 4;
+  uint8_t second : 4;
+} libgfx_4BitPixel;
+
+typedef struct {
+  uint8_t red : 5;
+  uint8_t green : 5;
+  uint8_t blue : 5;
+  uint8_t alpha : 1;
+} libgfx_16BitPixel;
+
+typedef struct {
+  uint8_t red;
+  uint8_t green;
+  uint8_t blue;
+} libgfx_24BitPixel;
+
+typedef struct {
+  uint8_t red;
+  uint8_t green;
+  uint8_t blue;
+  uint8_t alpha;
+} libgfx_32BitPixel;
+#pragma pack(pop)
+
+typedef int (*libgfx_ReadCallback)(void *dest, size_t size, void *userData);
+
+typedef int (*libgfx_WriteCallback)(const void *src, size_t size, void *userData);
+
+/**
+ * @brief Load an image from the given filename.
+ */
+libgfx_Image *libgfx_loadImageFromFile(const char *filename);
+
+/**
+ * @brief Load an image from memory using the given buffer.
+ */
+libgfx_Image *libgfx_loadImageFromMemory(const void *buffer, size_t bufferSize);
+
+/**
+ * @brief Load an image using provided callback.
+ */
+libgfx_Image *libgfx_loadImageFromCallback(libgfx_ReadCallback read, void *userData);
+
+/**
+ * @brief Write the image to the given file.
+ */
+int libgfx_writeImageToFile(libgfx_Image *image, const char *filename);
+
+/**
+ * @brief Write the image into memory.
+ */
+int libgfx_writeImageToMemory(libgfx_Image *image, void *buffer, size_t bufferSize);
+
+/**
+ * @brief Write the image using the provided callback.
+ */
+int libgfx_writeImageToCallback(libgfx_Image *image, libgfx_WriteCallback, void *userData);
+
+/**
+ * @brief Convert an indexed image to an RGB/RGBA image using the provided palette.
+ */
+int libgfx_applyPalette(libgfx_Image *image, libgfx_ColorFormat colorFormat, const void *colors);
+
+/**
+ * @brief Convert the image format.
+ * 
+ * Conversion is only allowed between indexed or non-indexed formats.
+ * Conversion from or to indexed and non-indexed is not allowed.
+ *
+ * @see libgfx_applyPalette() for converting an indexed image to an RGB/RGBA image.
+ */
+int libgfx_convertFormat(libgfx_Image *image, libgfx_ColorFormat colorFormat);
+
+/**
+ * @brief Create a new empty image using the given parameters.
+ */
+libgfx_Image *libgfx_createImage(int width, int height, libgfx_ColorFormat colorFormat, libgfx_ImageType type);
+
+/**
+ * @brief Allocate frames or tiles for the given image.
+ *
+ * This operation is only allowed once per image to initialize the frame data.
+ */
+int libgfx_allocFrames(libgfx_Image *image, int numFrames);
+
+/**
+ * @brief Allocate animations for the given image.
+ *
+ * This operation is only allowed once per image to initialize the animation data.
+ */
+int libgfx_allocAnimations(libgfx_Image *image, int numAnimations);
+
+/**
+ * @brief Allocate palettes for the given image.
+ *
+ * This operation is only allowed once per image to initialize the palette data.
+ */
+int libgfx_allocPalettes(libgfx_Image *image, int numPalettes);
+
+/**
+ * @brief Allocate animation.
+ *
+ * This operation is only allowed once per animation.
+ */
+int libgfx_allocAnimation(libgfx_Image *image, libgfx_Animation *animation, int numFrames);
+
+/**
+ * @brief Allocate palette.
+ *
+ * This operation is only allowed once per palette.
+ */
+int libgfx_allocPalette(libgfx_Image *image, libgfx_Palette *palette, libgfx_ColorFormat colorFormat, int numColors);
+
+/**
+ * @brief Get pointer to the given frame index.
+ */
+void *libgfx_getFramePointer(libgfx_Image *image, int frameIndex);
+
+libgfx_Animation *libgfx_getAnimation(libgfx_Image *image, int index);
+
+libgfx_Palette *libgfx_getPalette(libgfx_Image *image, int index);
+
+/**
+ * @brief Free memory that was allocated for this image.
+ *
+ * This also frees all memories allocated for frames, animations, palettes etc.
+ */
+void libgfx_freeImage(libgfx_Image *image);
+
+libgfx_4BitPixel *libgfx_to4BitPixels(void *pixels);
+
+libgfx_16BitPixel *libgfx_to16BitPixels(void *pixels);
+
+libgfx_24BitPixel *libgfx_to24BitPixels(void *pixels);
+
+libgfx_32BitPixel *libgfx_to32BitPixels(void *pixels);
+
+/**
+ * @brief Get a pointer to the latest error message string or NULL if there is none.
+ */
+const char *libgfx_getError();
+
+#ifdef __cplusplus
+}
+#endif
