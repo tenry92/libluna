73b9b2d78a49c4468b26a9bbb7ab8ec91cf231b7
Simon "Tenry" Burchert <contact@simon-burchert.com>
2025-09-24 20:28:41 +0200

    Fix InputManager test

--- libluna/InputManager.test.cpp
+++ libluna/InputManager.test.cpp
@@ -6,90 +6,55 @@ using namespace Luna;
 int main(int, char**) {
   TEST("isButtonPressed 0s", []() {
     InputManager inputManager;
-    std::queue<ButtonEvent> queue;
     inputManager.addButtonBinding("jump", "A");
 
     ASSERT(!inputManager.isButtonPressed("jump"), "!jump");
 
-    queue.emplace(ButtonEvent("A", true));
-    inputManager.update(&queue, 0.1f);
+    inputManager.handleButtonEvent(ButtonEvent("A", true));
     ASSERT(inputManager.isButtonPressed("jump"), "jump");
 
-    inputManager.update(&queue, 0.1f);
+    inputManager.update(0.1f);
     ASSERT(!inputManager.isButtonPressed("jump"), "no jump");
 
-    queue.emplace(ButtonEvent("A", false));
-    inputManager.update(&queue, 0.1f);
+    inputManager.handleButtonEvent(ButtonEvent("A", false));
+    inputManager.update(0.1f);
     ASSERT(!inputManager.isButtonPressed("jump"), "no longer jump");
   });
 
   TEST("isButtonPressed 0.5s", []() {
     InputManager inputManager;
-    std::queue<ButtonEvent> queue;
     inputManager.addButtonBinding("jump", "A");
     float buffer = 0.5f;
 
     ASSERT(!inputManager.isButtonPressed("jump", buffer), "!jump");
 
-    queue.emplace(ButtonEvent("A", true));
-    inputManager.update(&queue, 0.1f);
+    inputManager.handleButtonEvent(ButtonEvent("A", true));
+    inputManager.update(0.1f);
     ASSERT(inputManager.isButtonPressed("jump", buffer), "jump");
 
-    inputManager.update(&queue, 0.1f);
+    inputManager.update(0.1f);
     ASSERT(inputManager.isButtonPressed("jump", buffer), "still jump");
 
-    inputManager.update(&queue, buffer);
+    inputManager.update(buffer);
     ASSERT(!inputManager.isButtonPressed("jump", buffer), "no longer jump");
   });
 
   TEST("isButtonHeld 0.5s", []() {
     InputManager inputManager;
-    std::queue<ButtonEvent> queue;
     inputManager.addButtonBinding("jump", "A");
     float duration = 0.5f;
 
     ASSERT(!inputManager.isButtonHeld("jump", duration), "!jump");
 
-    queue.emplace(ButtonEvent("A", true));
-    inputManager.update(&queue, 0.1f);
+    inputManager.handleButtonEvent(ButtonEvent("A", true));
+    inputManager.update(0.1f);
     ASSERT(!inputManager.isButtonHeld("jump", duration), "not jump");
 
-    inputManager.update(&queue, 0.1f);
+    inputManager.update(0.1f);
     ASSERT(!inputManager.isButtonHeld("jump", duration), "still not jump");
 
-    inputManager.update(&queue, duration);
+    inputManager.update(duration);
     ASSERT(inputManager.isButtonHeld("jump", duration), "jump");
-  });
-
-  TEST("clear queue", []() {
-    InputManager inputManager;
-    std::queue<ButtonEvent> queue;
-    inputManager.addButtonBinding("jump", "A");
-
-    queue.emplace(ButtonEvent("A", true));
-    queue.emplace(ButtonEvent("A", false));
-    queue.emplace(ButtonEvent("A", true));
-    queue.emplace(ButtonEvent("A", false));
-    queue.emplace(ButtonEvent("B", true));
-    queue.emplace(ButtonEvent("B", false));
-    inputManager.update(&queue, 0.1f);
-    ASSERT_EQL(0, queue.size(), "queue is empty");
-  });
-
-  TEST("return unused events", []() {
-    InputManager inputManager;
-    std::queue<ButtonEvent> queue;
-    inputManager.setReturnUnused(true);
-    inputManager.addButtonBinding("jump", "A");
-
-    queue.emplace(ButtonEvent("A", true));
-    queue.emplace(ButtonEvent("A", false));
-    queue.emplace(ButtonEvent("A", true));
-    queue.emplace(ButtonEvent("A", false));
-    queue.emplace(ButtonEvent("B", true));
-    queue.emplace(ButtonEvent("B", false));
-    inputManager.update(&queue, 0.1f);
-    ASSERT_EQL(2, queue.size(), "queue contains 2 events");
   });
 
   return runTests();
